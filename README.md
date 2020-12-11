deb697cf24420ef4e68da8d4e251dee08cb3ccdf
Brody Spearman <BrodySpearman@gmail.com>
2020-12-11 13:25:14 -0600

    Initializing project and setting up framework for webpage.

---
+++ README.md
@@ -0,0 +1,1 @@
+With this project, I will be utilizing the DOM and javascript elements to create an etch-e-sketch in the browser.
---
+++ script.js
@@ -0,0 +1,5 @@
+const sketch = document.querySelector('#container');
+
+for (let i = 0; i < 16; i++) {
+    const etch = document.createElement(`div${i}`);
+}
